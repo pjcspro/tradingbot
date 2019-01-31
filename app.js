cd55ed74d93f28c291f44dea69b8410cff74b73d
Paulo Santos <pjcs.pro@gmail.com>
2019-01-31 20:25:20 +0000

    improved script

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const CRONJOB = {
 ///////// VARS /////////
 var binance = new Binance();
 var sll_order_id; //sll => Sell Loss Limit
+var sll_stop_price;
 
 if (!argv.config) {
   console.log("Configuration file not set");
@@ -39,34 +40,7 @@ console.log(
 
 ///////// METHODS /////////
 async function start() {
-  var price = await binance.price(config.symbol);
-  var price_last = price.last;
-
-  var stop_price = price_last - config.trigger_distance;
-  var limit_price = stop_price + config.limit_price_distance;
-
-  console.log("\n======== CREATING ORDER ======== ");
-  console.log(
-    asTable([
-      ["symbol", "amount", "limit_price", "stop_price"],
-      [config.symbol, config.amount, limit_price, stop_price]
-    ])
-  );
-
-  if (!global.DEBUG) {
-    var result = await binance.createOrderStopLimit(
-      config.symbol,
-      config.amount,
-      limit_price,
-      stop_price
-    );
-
-    //TODO: handle errors
-    //TODO: add action to log file
-    sll_order_id = result.id;
-    //console.log(result, sll_order_id);
-  }
-
+  runAlgorithm();
   /* constructor(cronTime, onTick, onComplete, start, timezone, context, runOnInit, unrefTimeout) */
   var cron = new cronJob(
     CRONJOB.TIME.EVERY_MINUTE,
@@ -79,16 +53,52 @@ async function start() {
 }
 
 async function runAlgorithm() {
+  //TODO: Maybe only update if diff threshold
   var price = await binance.price(config.symbol);
-  console.log("price", price.last);
+  var price_last = price.last;
 
-  console.log("\n======== CANCELLING ORDER ======== ");
-  if (!global.DEBUG) {
-    var result = await binance.cancelOrder(sll_order_id, config.symbol);
-    console.log(result);
+  var stop_price = price_last - config.trigger_distance;
+  var limit_price = stop_price + config.limit_price_distance;
 
-    //TODO: create new order with updated price
-    //TODO: Maybe only update if diff threshold
+  if (price_last <= sll_stop_price) {
+    //CHECK IF ORDER STILL OPENED
+    //TODO: This
+  }
+
+  //TODO: sll_stop_price + threshold?? how know threshold?
+  if (stop_price > sll_stop_price) {
+    //MOVE STOP LOSS ABOVE
+
+    sll_stop_price = stop_price;
+    console.log("\n======== CANCELLING ORDER ======== ");
+    if (!global.DEBUG) {
+      if (sll_order_id) {
+        var result = await binance.cancelOrder(sll_order_id, config.symbol);
+        console.log(result);
+      }
+    }
+
+    console.log("\n======== CREATING ORDER ======== ");
+    console.log(
+      asTable([
+        ["symbol", "amount", "limit_price", "stop_price"],
+        [config.symbol, config.amount, limit_price, stop_price]
+      ])
+    );
+
+    if (!global.DEBUG) {
+      var result = await binance.createOrderStopLimit(
+        config.symbol,
+        config.amount,
+        limit_price,
+        stop_price
+      );
+
+      //TODO: handle errors
+      //TODO: add action to log file
+      sll_order_id = result.id;
+      //console.log(result, sll_order_id);
+    }
   }
 }
 
